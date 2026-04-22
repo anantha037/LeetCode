e95095ae68cecf20122a3020245520b9f1b64e6e
anantha037 <ananthan7703@gmail.com>
2026-04-22 12:13:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,20 +1,22 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        obj = {}
         res = []
-        for word in dictionary:
-            obj[word] = list(word)
-        
-        for word in queries:
+        obj = set(dictionary)
+        for i in range(len(queries)):
+            word = queries[i]
             if word in obj:
                 res.append(word)
-            else:
-                for arr in obj.values():
-                    arr2 = list(word)
-                    if sum(el1 != el2 for el1, el2 in zip(arr, arr2)) > 2:
-                        continue
-                    else:
-                        res.append(word)
+                continue
+            for j in range(len(dictionary)):
+                check = dictionary[j]
+                diff = 0
+                for a,b in zip(word, check):
+                    if diff>2:
                         break
-
+                    if a!=b:
+                        diff +=1
+                if diff<=2:
+                    res.append(word)
+                    break
+    
         return res
