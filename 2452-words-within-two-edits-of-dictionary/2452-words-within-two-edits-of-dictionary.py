b1f54b236681bc9026f2087a7a397755671e6142
anantha037 <ananthan7703@gmail.com>
2026-04-22 12:11:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,19 +1,17 @@
-import numpy as np
-
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
         obj = {}
         res = []
         for word in dictionary:
-            obj[word] = np.array(list(word))
+            obj[word] = list(word)
         
         for word in queries:
             if word in obj:
                 res.append(word)
             else:
                 for arr in obj.values():
-                    arr2 = np.array(list(word))
-                    if np.sum(arr2 != arr) > 2:
+                    arr2 = list(word)
+                    if sum(el1 != el2 for el1, el2 in zip(arr, arr2)) > 2:
                         continue
                     else:
                         res.append(word)
