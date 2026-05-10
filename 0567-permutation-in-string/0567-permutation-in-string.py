3a3b580e2dcd52d417f602cbce201d641acbc9b7
anantha037 <ananthan7703@gmail.com>
2026-05-10 20:04:02 +0530

    Time: 545 ms (14.62%), Space: 19.4 MB (54.13%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        count1 = Counter(s1)
+
+        n = len(s1)
+        i = 0
+
+        while i<=len(s2)-n:
+            if s2[i] in count1:
+                word = s2[i:n+i]
+                count2 = Counter(word)
+
+                for j in range(n):
+                    if word[j] not in count1:
+                        i += j+1
+                        break
+                    else:
+                        if count2[word[j]] > count1[word[j]]:
+                            i += j+1
+                            break
+                else:
+                    return True
+            else:
+                i+=1
+        return False
