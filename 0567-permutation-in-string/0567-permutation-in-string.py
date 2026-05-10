601ca67eb13bb988e975062d2deddb2073eef17f
anantha037 <ananthan7703@gmail.com>
2026-05-10 20:25:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,27 +2,22 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        
-        count1 = Counter(s1)
-
         n = len(s1)
-        i = 0
+        freq1 = [0] * 26
+        freq2 = [0] * 26
 
-        while i<=len(s2)-n:
-            if s2[i] in count1:
-                word = s2[i:n+i]
-                count2 = Counter(word)
+        for i in range(n):
+            freq1[ord(s1[i])-97] += 1
+            freq2[ord(s2[i])-97] += 1
+        
+        if freq1 == freq2:
+            return True
+        for i in range(n,len(s2)):
+            freq2[ord(s2[i-n])-97] -=1
+            freq2[ord(s2[i]) - 97] +=1
 
-                for j in range(n):
-                    if word[j] not in count1:
-                        i += j+1
-                        break
-                    else:
-                        if count2[word[j]] > count1[word[j]]:
-                            i += j+1
-                            break
-                else:
-                    return True
-            else:
-                i+=1
+            if freq1 == freq2:
+                return True
+            
         return False
+        
