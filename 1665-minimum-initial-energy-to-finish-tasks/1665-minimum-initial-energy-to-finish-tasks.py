c2f85d3db515d11574f7cd366187465aeb4e8fcd
anantha037 <ananthan7703@gmail.com>
2026-05-12 21:15:26 +0530

    Time: 79 ms (28.71%), Space: 55.6 MB (60.4%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        total = 0
+        
+        sorted_data = sorted(tasks, key=lambda x: abs(x[0] - x[1]),reverse=True)
+        
+        answer = [sorted_data[0][0],sorted_data[0][1]]
+
+        for i in range(1, len(sorted_data)):
+            check = answer[1]-answer[0]
+            if check<sorted_data[i][1]:
+                diff = sorted_data[i][1]-check
+                answer[1] += diff
+            answer[0] += sorted_data[i][0]
+
+        return answer[1]
