0b12fbbfe5a5b48558fd9a6aaf2f702ce4419591
anantha037 <ananthan7703@gmail.com>
2026-05-14 10:33:46 +0530

    Time: 1 ms (39.15%), Space: 19.3 MB (17.79%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,15 +1,22 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        obj = Counter(nums)
-
-        max_val = max(obj)
         
-        if obj[max_val] != 2:
+        freq = {}
+        
+        max_val = float('-inf')
+
+        times = 0
+
+        for num in nums:
+            if num>max_val:
+                max_val = num
+            freq[num] = freq.get(num,0) + 1
+            if freq[num] == 2:
+                times +=1
+            if times>1:
+                return False
+
+        if freq[max_val] != 2 or len(nums) != max_val+1:
             return False
-        elif len(nums) != max_val+1:
-            return False
-        else:
-            for i in obj:
-                if i != max_val and obj[i] != 1:
-                    return False
+
         return True
