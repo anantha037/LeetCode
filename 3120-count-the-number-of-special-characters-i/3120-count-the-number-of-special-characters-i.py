7ecfd3a58897004a336e5b7047476e0a04fd68e3
Anantha Krishnan K <ananthan7703@gmail.com>
2026-05-26 10:30:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (25.31%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,11 +1,9 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         count = 0
-        checked = set()
-        for i in word:
-            if i.islower() and i not in checked:
-                checked.add(i)
-                val = ord(i)-32
-                if chr(val) in word:
-                    count+=1
+        freq = Counter(word)
+
+        for i in freq:
+            if (i.isupper() and i.lower in freq) or (i.islower() and i.upper() in freq):
+                count +=1
         return count
