d4a34556483bfaf5c942d71355ac289ba5372426
Anantha Krishnan K <ananthan7703@gmail.com>
2026-05-26 10:45:17 +0530

    Time: 2 ms (42.11%), Space: 19.4 MB (25.31%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -4,6 +4,6 @@ class Solution:
         freq = Counter(word)
 
         for i in freq:
-            if (i.isupper() and i.lower in freq) or (i.islower() and i.upper() in freq):
+            if i.isupper() and i.lower() in freq:
                 count +=1
         return count
