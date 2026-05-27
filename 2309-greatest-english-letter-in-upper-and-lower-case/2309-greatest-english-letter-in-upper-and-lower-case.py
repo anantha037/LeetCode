076ef6656afb6bcabef6cd9f165ad0d7d67c5cf1
Anantha Krishnan K <ananthan7703@gmail.com>
2026-05-27 12:40:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.7%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -4,7 +4,7 @@ class Solution:
         if len(s)==1:
             return ''
 
-        for i in range(90,64,-1):
+        for i in range(ord('Z'),ord('A')-1,-1):
             if chr(i) in s and chr(i+32) in s:
                 return chr(i)
 
