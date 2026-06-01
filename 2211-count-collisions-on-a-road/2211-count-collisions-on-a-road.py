06ad20afdf371c46200a0ff0470efa52e3279083
Anantha Krishnan K <ananthan7703@gmail.com>
2026-06-01 17:42:16 +0530

    Time: 0 ms (100%), Space: 19.9 MB (80.54%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -1,29 +1,6 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        stack = []
+        directions = directions.lstrip('L')
+        directions = directions.rstrip('R')
 
-        answer = 0
-
-        for car in directions:
-            if car =='L':
-                if stack and stack[-1]!=car:
-                    if stack[-1]=='S':
-                        answer +=1
-                        continue
-                    else:
-                        answer +=2
-                        stack.pop()
-                        while stack and stack[-1]=='R':
-                            stack.pop()
-                            answer +=1
-                        stack.append('S')
-    
-                if not stack or stack[-1]=='S':
-                    continue
-            elif car == 'S':
-                while stack and stack[-1] == 'R':
-                    stack.pop()
-                    answer +=1
-            
-            stack.append(car)
-        return answer
+        return directions.count('R') + directions.count('L')
