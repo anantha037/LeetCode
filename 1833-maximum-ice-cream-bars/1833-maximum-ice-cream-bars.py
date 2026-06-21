75534da427e64745d215be7fab720188009a3426
Anantha Krishnan K <ananthan7703@gmail.com>
2026-06-21 18:56:51 +0530

    Time: 78 ms (70.7%), Space: 32.1 MB (22.79%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,25 +1,6 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        def count_sort(arr):
-            if not arr:
-                return []
-            n = len(arr)
-            max_val = max(arr)
-            cntArr = [0]*(max_val+1)
-            for i in arr:
-                cntArr[i] +=1
-
-            for i in range(1,len(cntArr)):
-                cntArr[i] += cntArr[i-1]
-
-            ans = [0]*n
-
-            for i in range(n-1,-1,-1):
-                ans[cntArr[arr[i]]-1]=arr[i]
-                cntArr[arr[i]]-=1
-
-            return ans
-        costs = count_sort(costs)
+        costs.sort()
         total = 0
 
         for i in costs:
