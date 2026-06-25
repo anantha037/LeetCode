9296fa89b031940434f350643b6f82c8403e16e6
Anantha Krishnan K <ananthan7703@gmail.com>
2026-06-25 13:38:32 +0530

    Time: 1701 ms (59.11%), Space: 19.1 MB (98.67%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.py
@@ -4,16 +4,9 @@ class Solution:
         count = obj[target]
 
         n = len(nums)
-        min_len = 1
         max_len = min(count*2-1,n)
-        if max_len<0:
-            return 0
-        elif max_len==1:
+        if count<=1:
             return count
-        # elif max_len==n:
-        #     print('HI')
-        #     return n*(n+1)//2
-        
         total =0
 
         for i in range(len(nums)):
@@ -23,8 +16,6 @@ class Solution:
                     curr+=1
                 if 2*curr>j-i+1:
                     total+=1
-                    # print(i,j,total)
-            # print(i,curr,total)
         return total
 
             
