3076e8271f694faf0c4dbabeb32f5fa170e7b748
Anantha Krishnan K <ananthan7703@gmail.com>
2026-07-14 12:51:18 +0530

    Time: 7 ms (18.48%), Space: 20.6 MB (9.65%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -11,7 +11,7 @@ class Solution:
                 return True,val+1
             left = height(node.left,val)
             right = height(node.right,val)
-            print(left,node.val,right)
+
             if (left[0] and right[0]) and abs(left[1]-right[1])<2:
                 return True,max(left[1],right[1])+1
             return False,max(left[1],right[1])+1
