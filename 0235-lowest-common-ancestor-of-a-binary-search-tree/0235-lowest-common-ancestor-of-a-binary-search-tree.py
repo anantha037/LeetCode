9555af1f25fb3b4663a4b8bc0ff8aa5a599d978f
Anantha Krishnan K <ananthan7703@gmail.com>
2026-07-17 12:35:22 +0530

    Time: 67 ms (45.96%), Space: 22.9 MB (17.58%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,26 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(node,target,ancestors):
-            ancestors.append(node)
-            if node==target:
-                return ancestors
-            if node.val<target.val:
-                return dfs(node.right,target,ancestors)
+        curr = root
+        while curr:
+            if p.val>curr.val and q.val>curr.val:
+                curr = curr.right
+            elif p.val<curr.val and q.val<curr.val:
+                curr = curr.left
             else:
-                return dfs(node.left,target,ancestors)
-        
-        first = dfs(root,p,[])
-        second = dfs(root,q,[])
-
-        if len(first)>len(second):
-            first,second = second,first
-
-        second = set(second)
-
-        for i in range(len(first)-1,-1,-1):
-            if first[i] in second:
-                return first[i]
+                return curr
             
 
             
