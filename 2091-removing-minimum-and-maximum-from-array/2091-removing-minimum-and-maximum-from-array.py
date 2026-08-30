344946bdf5fa2ef114e19e18eec55ff4d6accc64
Anantha Krishnan K <ananthan7703@gmail.com>
2026-08-30 23:58:21 +0530

    Time: 8 ms (96.37%), Space: 33.6 MB (35.35%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -6,33 +6,11 @@ class Solution:
         max_val = nums.index(max(nums))+1
         n = len(nums)
 
-        if min_val<n-min_val+1:
-            min_del = min_val
-            min_opp = False
-        else:
-            min_del = n-min_val+1
-            min_opp=True
-        
-        if max_val<n-max_val+1:
-            max_del = max_val
-            max_opp = False
-        else:
-            max_del = n-max_val+1
-            max_opp = True
-        
-        if min_opp!=max_opp:
-            if min_del<max_del:
-                if max_opp:
-                    res = min(min_del+max_del,max_val)
-                else:
+        if min_val>max_val:
+            min_val,max_val = max_val,min_val
+ 
+        front = max_val
+        back = n-min_val+1
+        both = min_val+(n-max_val+1)
+        return min(front,back,both)
 
-                    res = min(max_del+min_del,n-max_val+1)
-            else:
-                if max_opp:
-                    res = min(min_del+max_del,n-min_val+1)
-                else:
-                    
-                    res = min(max_del+min_del,min_val)
-            return res
-        else:
-            return max(min_del,max_del)
